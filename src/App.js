7238cf7b16ef7a682b1a5caea1d8a1a90a1e5cb6
ydrea <ydrea.wrld@gmail.com>
2024-10-01 12:50:46 +0200

    recursive react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import icon from './folder.ico';
 function App() {
-  return (
+let folders = [
+  {name: "Home"}, 
+   {name: "About", folders:[{ name: "Company"}, { name: "Team"}], 
+   },
+   { name: "Contact", folders:[{name: "Email", 
+    folders: [{ name: "a@b.com"}, {name: "b@c.com"},]}, {name: "Phone"}, { name: "Address"}], 
+   }, 
+   {name: "Blog", folders: [{name: "Posts"}, {name: "Comments", folders: [{name: "a"}, {name: "b"}]}]}, 
+   {name: "Help", folders: [{name: "FAQ"}, {name: "Support"}]},
+   ]
+  
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <ul>
+      {folders.map((folder) => 
+      <li key={folder.name}>
+     <span> <img src={icon}/>{folder.name}</span>
+      <ul>
+        {folder.folders?.map((folder) => 
+        <Folder folder={folder} key={folder.name} />)}
+      </ul>
+      </li>)}
+        </ul>
     </div>
   );
 }
-
 export default App;
+
+function Folder ({folder}) {
+  return (
+    <li key={folder.name}>
+      <span> <img src={icon}/>{folder.name}</span>
+ {folder.folders?.map((folder) =>
+ <Folder folder={folder} />)}
+    </li>
+  )
+}
